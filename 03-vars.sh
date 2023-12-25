8e7e081d9f8d7185872e4ad1d4adeb36a725f1c2
maddikiri malleswari <malleswrimaddikiri@gmail.com>
2023-12-25 22:07:09 +0530

    expense scripts

---
+++ 03-vars.sh
@@ -0,0 +1,10 @@
+# if we name some data it is called as variable.
+# syntax to declare variable
+# var_name = data
+a=10
+x=100
+y=true
+name=devops
+
+# how to access variable.
+echo value of a = $a
