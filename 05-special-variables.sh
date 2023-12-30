3fe8c05f2435b8b0773690d63dc70c8536eebb98
maddikiri malleswari <malleswrimaddikiri@gmail.com>
2023-12-30 12:04:15 +0530

    expense scripts

---
+++ 05-special-variables.sh
@@ -0,0 +1,8 @@
+# special variables are 0-n,@,*,#,?
+ echo script name     - $0
+ echo first argument  - $1
+ echo second argument - $2
+ echo all arguments   - $@
+ echo all arguments   - $*
+ echo no.of arguments - $#
+
