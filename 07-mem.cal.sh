7283158425841dd153cd5276d4f18ad513263838
maddikiri malleswari <malleswrimaddikiri@gmail.com>
2024-01-13 22:53:25 +0530

    expense scripts

--- 07-mem.cal.sh
+++ 07-mem.cal.sh
@@ -2,3 +2,15 @@ total=$(free | grep Mem | awk '{print$2}')
 free=$( free | grep Mem | awk '{print$4}')
 free_percentage=$(echo "$free / $total*100" | bc -l | awk -F . '{print$1}')
 echo $free_percentage
+
+# 0-50 used good
+# 51-70 used orange
+#>70 red
+
+if [ $used_percentage -lt 50 ];then
+   echo Mem stat is green
+elif [ $used_percentage -lt 70 ];then
+   echo Mem stat is orange
+else
+   echo Mem stat is red
+fi
